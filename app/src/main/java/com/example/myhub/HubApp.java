03a2f46048e1994f88b18c6f8f90cb0effdf3966
han1254 <you@example.com>
2020-02-17 15:30:19 +0800

    修改了

--- app/src/main/java/com/example/myhub/HubApp.java
+++ app/src/main/java/com/example/myhub/HubApp.java
@@ -5,12 +5,6 @@ import android.content.Context;
 
 import com.example.base.BaseApplication;
 import com.example.base.nettype.netchange.NetChangeWatcherUtil;
-import com.example.myhub.mvvm.di.componet.DaggerGlobalComponent;
-import com.example.myhub.mvvm.di.componet.DaggerSearchComponent;
-import com.example.myhub.mvvm.di.componet.DaggerSplashComponent;
-import com.example.myhub.mvvm.di.componet.GlobalComponent;
-import com.example.myhub.mvvm.di.componet.SearchComponent;
-import com.example.myhub.mvvm.di.componet.SplashComponent;
 
 /**
  * Time:2020/2/10 8:28
@@ -22,18 +16,14 @@ public class HubApp extends BaseApplication {
 
     private static Context context;
     private static Application mApplication;
-    private SplashComponent mSplashComponent;
-    private GlobalComponent mGlobalComponent;
-    private SearchComponent mSearchComponent;
+
 
     @Override
     public void onCreate() {
         super.onCreate();
         NetChangeWatcherUtil.getInstance().init(this);
         context = getApplicationContext();
-        mSplashComponent = DaggerSplashComponent.create();
-        mGlobalComponent = DaggerGlobalComponent.create();
-        mSearchComponent = DaggerSearchComponent.create();
+
         mApplication = this;
     }
 
@@ -41,17 +31,6 @@ public class HubApp extends BaseApplication {
         return context;
     }
 
-    public SplashComponent getSplashComponent() {
-        return mSplashComponent;
-    }
-
-    public GlobalComponent getGlobalComponent() {
-        return mGlobalComponent;
-    }
-
-    public SearchComponent getSearchComponent() {
-        return mSearchComponent;
-    }
 
     public static Application getApplication() {
         return mApplication;
